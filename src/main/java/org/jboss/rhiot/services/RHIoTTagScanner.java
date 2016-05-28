1b0c167f46326d4ca5fad248f17ebee9d4c6dad1
starksm64 <starksm64@gmail.com>
2016-05-27 17:24:52 -0700

    Move native library load into HCIDump class

--- src/main/java/org/jboss/rhiot/services/RHIoTTagScanner.java
+++ src/main/java/org/jboss/rhiot/services/RHIoTTagScanner.java
@@ -10,7 +10,6 @@ import org.eclipse.kura.configuration.ConfigurableComponent;
 
 import java.nio.ByteOrder;
 import java.util.Date;
-import java.util.Dictionary;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -21,12 +20,8 @@ import java.util.Map;
 public class RHIoTTagScanner implements ConfigurableComponent, IRHIoTTagScanner, IAdvertEventCallback {
     private static final String APP_ID = "org.jboss.rhiot.services.RHIoTTagScanner";
     private Map<String, Object> properties;
+    private volatile boolean scannerInitialized;
 
-    protected void activate(ComponentContext componentContext) {
-        System.out.printf("RHIoTTagScanner.activate; Bundle " + APP_ID + " has started!\n");
-        Dictionary<String,Object> dict = componentContext.getProperties();
-        System.out.printf("hciDev=%s\n", dict.get("hciDev"));
-    }
     @Override
     public boolean advertEvent(AdEventInfo info) {
         System.out.printf("+++ advertEvent, rssi=%d, time=%s\n", info.getRssi(), new Date(info.getTime()));
@@ -37,6 +32,7 @@ public class RHIoTTagScanner implements ConfigurableComponent, IRHIoTTagScanner,
         return false;
     }
 
+    
     protected void activate(ComponentContext componentContext, Map<String, Object> properties) {
         System.out.printf("RHIoTTagScanner.activate2; Bundle " + APP_ID + " has started with: %s\n", properties);
         System.out.printf("hciDev=%s\n", properties.get("hciDev"));
@@ -46,14 +42,16 @@ public class RHIoTTagScanner implements ConfigurableComponent, IRHIoTTagScanner,
     protected void deactivate(ComponentContext componentContext) {
         HCIDump.setAdvertEventCallback(null);
         HCIDump.freeScanner();
+        scannerInitialized = false;
         System.out.printf("RHIoTTagScanner.deactivate; Bundle " + APP_ID + " has stopped!\n");
     }
 
     public void updated(Map<String, Object> properties) {
         System.out.printf("RHIoTTagScanner.updated; Bundle " + APP_ID + " has stopped!\n");
         String hciDev = properties.get("hciDev").toString();
+        boolean debugMode = (Boolean) properties.get("hcidumpDebugMode");
         System.out.printf("hciDev=%s\n", hciDev);
-        loadLibrary();
+        HCIDump.loadLibrary();
         this.properties = properties;
         if(properties != null && !properties.isEmpty()) {
             Iterator<Map.Entry<String, Object>> it = properties.entrySet().iterator();
@@ -63,18 +61,13 @@ public class RHIoTTagScanner implements ConfigurableComponent, IRHIoTTagScanner,
             }
         }
         // Setup scanner
+        HCIDump.enableDebugMode(debugMode);
         HCIDump.setAdvertEventCallback(this);
-        HCIDump.initScanner(hciDev, 512, ByteOrder.BIG_ENDIAN);
-    }
-
-    private void loadLibrary() {
-        System.out.printf("Loading scannerJni library...\n");
-        try {
-            System.loadLibrary("scannerJni");
-            System.out.printf("done\n");
-        }catch (Exception e) {
-            System.out.printf("Failed:\n");
-            e.printStackTrace();
+        if(scannerInitialized == false) {
+            HCIDump.initScanner(hciDev, 512, ByteOrder.BIG_ENDIAN);
+        } else {
+            System.out.printf("Scanner already iniatialized, skipped HCIDump.initScanner\n");
         }
+        scannerInitialized = true;
     }
 }
