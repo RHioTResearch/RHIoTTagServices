094c89122f19f1acecd099c42c4e8ce9a1916d0c
starksm64 <starksm64@gmail.com>
2016-06-13 19:15:59 -0700

    Add constants for the msg metrics and rest endpoints

--- src/main/java/org/jboss/rhiot/services/api/IRHIoTTagScanner.java
+++ src/main/java/org/jboss/rhiot/services/api/IRHIoTTagScanner.java
@@ -4,4 +4,36 @@ package org.jboss.rhiot.services.api;
  * Created by sstark on 5/25/16.
  */
 public interface IRHIoTTagScanner {
+   // Metrics keys
+   // Set 1, the tag data sent on every advertisement event
+   static final String TAG_TEMP = "rhiotTag.temperature";
+   static final String TAG_KEYS = "rhiotTag.keys";
+   static final String TAG_LUX = "rhiotTag.lux";
+   // Set 2, the game state information sent on an event such as a key press or state timeout
+   static final String TAG_PREV_STATE = "rhiotTag.prevState";
+   static final String TAG_NEW_STATE = "rhiotTag.newState";
+   static final String TAG_EVENT = "rhiotTag.event";
+   // Set 3, the game progress information sent while the game is active.
+   static final String TAG_GAME_TIME_LEFT = "rhiotTag.gameTimeLeft";
+   static final String TAG_GAME_SCORE = "rhiotTag.gameScore";
+   static final String TAG_SHOOTING_TIME_LEFT = "rhiotTag.shootingTimeLeft";
+   static final String TAG_SHOTS_LEFT = "rhiotTag.shotsLeft";
+   // Set 4, the information about a hit on the light sensor sent when a sensor reading above a threshold value is detected
+   static final String TAG_HIT_SCORE = "rhiotTag.hitScore";
+   static final String TAG_HIT_RINGS_OFF_CENTER = "rhiotTag.hitRingsOffCenter";
+   // Set 5, the information about the game scores sent on each game end
+   static final String GW_LAST_GAME_SCORE = "rhiotTagGW.score";
+   static final String GW_LAST_GAME_SCORE_HITS = "rhiotTagGW.hits";
+   static final String GW_LAST_GAME_SCORE_TAG_ADDRESS = "rhiotTagGW.scoreTagAddress";
+   // Set 6, the information about the high game scores sent on game end when a new high score is achieved
+   static final String GW_HIGH_GAME_SCORE = "rhiotTagGW.highScore";
+   static final String GW_HIGH_GAME_SCORE_HITS = "rhiotTagGW.highScoreHits";
+   static final String GW_HIGH_GAME_SCORE_TAG_ADDRESS = "rhiotTagGame.highScoreTagAddress";
+
+
+   // REST endpoints
+   String CLOUD_PW_PATH = "/cloud-password";
+   String TAG_INFO_PATH = "/tags";
+   String GAMESM_DIGRAPH_PATH = "/gamesm-digraph";
+   String GAMESM_INFO_PATH = "/gamesm";
 }
